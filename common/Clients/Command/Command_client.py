88377f1c4060cda9d2b96c99dc74fb32cb447165
Kapil Mathur <kapil.mathur@rackspace.com>
2020-07-05 16:20:54 +0530

    New module for client methods of command module

---
+++ common/Clients/Command/Command_client.py
@@ -0,0 +1,26 @@
+"""The base client class for all the methods of
+/command endpoint of OP5 monitor"""
+
+from dynaconf import settings as conf
+
+
+class CommandBaseClient(object):
+    _suffix = 'command'
+
+    def __init__(self, client, base_url=None):
+        self.base_url = base_url
+        if base_url is None:
+            self.base_url = conf.OP5_BASE_URL
+        self.url = f'{self.base_url}/{self._suffix}'
+        self.client = client
+
+    _command = 'ACKNOWLEDGE_HOST_PROBLEM'
+
+    def acknowledge_host_problem(self, hostname, sticky, notify,
+                                 persistent, comment):
+
+        host_payload = dict(host_name=hostname, sticky=sticky, notify=notify,
+                            persistent=persistent, comment=comment)
+
+        full_url = f'{self.url}/{self._command}'
+        return self.client.post(url=full_url, data=host_payload)
