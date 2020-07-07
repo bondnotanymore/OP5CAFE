c15c722bfa4e4b6c9c13ce7c793dcc6f3fc94722
Kapil Mathur <kapil.mathur@rackspace.com>
2020-07-07 15:39:24 +0530

    Minor change

--- common/tools/Ansible/Ansible.py
+++ common/tools/Ansible/Ansible.py
@@ -54,10 +54,10 @@ class ansible_runner:
 
 if __name__ == '__main__':
 
-    path_to_ansible = '/Users/kapilmathur/My_ansible_journey/ping_with_ansible'
+    path_to_ansible = '/Users/kapilmathur/My_ansible_journey/playbooks'
     path_to_inventory = os.path.join(path_to_ansible, 'inventory.txt')
     path_to_playbook = path_to_ansible
-    playbook_name = 'default_ssh_port.yaml'
+    playbook_name = 'install_docker.yaml'
 
     pb_runner = ansible_runner(
         inv_location=path_to_inventory, pb_location=path_to_ansible,
