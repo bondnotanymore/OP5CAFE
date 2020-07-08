988517618c8de0bea3174cec2623ae7259ada40d
Kapil Mathur <kapil.mathur@rackspace.com>
2020-07-08 22:32:12 +0530

    New test for services

--- tests/Configuration/test_service_config.py
+++ tests/Configuration/test_service_config.py
@@ -109,7 +109,7 @@ class TestServiceConfig(ConfigBaseFixture):
                                      object_type='service',
                                      object_name=f'{hostname};{description}')
 
-        # Delete the host
+        # Delete the service
 
         r = self.cb.delete_service(description=description)
 
@@ -132,3 +132,96 @@ class TestServiceConfig(ConfigBaseFixture):
         service_list_data = r.json()
         service_name_list = [service['name'] for service in service_list_data]
         assert f'{hostname};{description}' not in service_name_list
+
+    def test_host_deletion_deletes_service(self):
+        # Add a new custom host to the OP5 monitor.
+
+        host_info = self.create_new_host(name=self.random_string(),
+                                         maxcheckattempts=2,
+                                         hostaddress=conf.CONFIG_HOST_ADDR,
+                                         command='check_ssh',
+                                         commandargs=20, checkinterval=2,
+                                         retryinterval=2, activechecks=True)
+
+        host_name = host_info['host_name']
+
+        # Adding 2 new services to the host
+
+        # Lets prepare the new service parameters first that we would
+        # like to send to the config API endpoint
+
+        command = 'check_ping'
+        checkinterval = 2
+        hostname = host_name
+        maxcheckattempts = 3
+        retryinterval = 2
+        description1 = self.random_string(prefix='Ping services')
+        displayname = f'Ping services for host: {host_name}'
+        activechecks = True
+
+        r = self.cb.add_service(command=command,
+                                checkinterval=checkinterval,
+                                hostname=hostname,
+                                maxcheckattempts=maxcheckattempts,
+                                retryinterval=retryinterval,
+                                description=description1,
+                                displayname=displayname,
+                                activechecks=activechecks,
+                                )
+
+        assert r.status_code == 201
+
+        # Adding another service to check the status of a webserver
+        # on the remote host.
+
+        command = 'check_http'
+        checkinterval = 2
+        hostname = host_name
+        maxcheckattempts = 2
+        retryinterval = 2
+        description2 = self.random_string(prefix='ApacheService')
+        displayname = f'Check http service on host: {host_name}'
+        activechecks = True
+
+        r = self.cb.add_service(command=command,
+                                checkinterval=checkinterval,
+                                hostname=hostname,
+                                maxcheckattempts=maxcheckattempts,
+                                retryinterval=retryinterval,
+                                description=description2,
+                                displayname=displayname,
+                                activechecks=activechecks,
+                                )
+
+        # Lets confirm these changes to register in the config files
+        # and the monitor service to restart
+
+        self.commit_to_configuration(change_type='new', object_type='service',
+                                     object_name=f'{host_name};{description2}')
+
+        # Now lets delete the host and that should also delete
+        # the associated services.
+
+        r = self.cb.delete_host(host_name=host_name)
+
+        assert r.status_code == 200
+
+        # Lets confirm this change to register in the config files
+        # and the monitor service to restart
+
+        r = self.cb.get_config_changes_to_save()
+        assert r.status_code == 200
+        r = self.cb.save_config_changes()
+        assert r.status_code == 200
+
+        # Verify that the deleted service is now not part of the existing
+        # host list monitored by OP5
+
+        r = self.cb.list_all_services()
+
+        assert r.status_code == 200
+
+        service_list_data = r.json()
+        service_name_list = [service['name'] for service in service_list_data]
+        assert f'{host_name};{description1}' not in service_name_list
+        assert f'{host_name};{description2}' not in service_name_list
