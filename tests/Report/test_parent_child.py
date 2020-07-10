17e4f2589a3579849f9d41c5c3ba59aaf734cca6
Kapil Mathur <kapil.mathur@rackspace.com>
2020-07-10 18:45:46 +0530

    Updated the flow

--- tests/Report/test_parent_child.py
+++ tests/Report/test_parent_child.py
@@ -51,6 +51,83 @@ class TestHostParentChild(OP5Fixture):
                                                 activechecks=True)
 
         parent_host_name = parent_host_info['host_name']
+        self._hosts_to_delete.append(parent_host_name)
+        time.sleep(5)
+
+        # Sleeping till the next check
+        object_type = '[hosts]'
+        query = f'name="{parent_host_name}"'
+        next_check_in_for_parent = self.how_much_to_sleep(
+            object_type=object_type, query=f'name="{parent_host_name}"'
+        )
+        logging.info(f'OP5 will do next check in : {next_check_in_for_parent}')
+        time.sleep(next_check_in_for_parent + 10)
+
+        # Get report in terms of list views from the filter API
+        full_parent_query = ''.join(
+            [object_type, f'name="{parent_host_name}"']
+        )
+        logging.info(full_parent_query)
+        columns = f'state,state_text,has_been_checked,state_type,' \
+                  f'state_type_text,plugin_output'
+        logging.info(columns)
+
+        r = self.fb.get_filter_query_data(query=full_parent_query,
+                                          columns=columns)
+
+        assert r.status_code == 200
+        list_view_data = r.json()[0]
+        logging.info(list_view_data)
+        assert list_view_data['has_been_checked'] == 1
+        assert list_view_data['state'] == 0
+        assert list_view_data['state_type'] == 1
+        assert list_view_data['state_text'] == 'up'
+        assert list_view_data['state_type_text'] == 'hard'
+
+        # Lets now patch the parent host with a check
+        # that's not going to pass and will make it go down
+
+        r = self.cb.patch_host_details(host_name=parent_host_name,
+                                       check_command='check_adam',
+                                       )
+        assert r.status_code == 200
+
+        self.commit_to_configuration(change_type='change',
+                                     object_name=parent_host_name,
+                                     object_type='host')
+
+        # Sleeping till the next check
+        object_type = '[hosts]'
+        query = f'name="{parent_host_name}"'
+        next_check_in_for_parent = self.how_much_to_sleep(
+            object_type=object_type, query=f'name="{parent_host_name}"'
+        )
+        logging.info(f'OP5 will do next check in : {next_check_in_for_parent}')
+        time.sleep(next_check_in_for_parent + 10)
+
+        # Get report in terms of list views from the filter API
+        full_parent_query = ''.join(
+            [object_type, f'name="{parent_host_name}"']
+        )
+        logging.info(full_parent_query)
+        columns = f'state,state_text,has_been_checked,state_type,' \
+                  f'state_type_text,plugin_output'
+        logging.info(columns)
+
+        r = self.fb.get_filter_query_data(query=full_parent_query,
+                                          columns=columns)
+
+        assert r.status_code == 200
+        list_view_data = r.json()[0]
+        logging.info(list_view_data)
+        assert list_view_data['has_been_checked'] == 1
+        assert list_view_data['state'] == 1
+        assert list_view_data['state_type'] == 0
+        assert list_view_data['state_text'] == 'down'
+        assert list_view_data['state_type_text'] == 'soft'
+
+        # Adding a new child, with a valid command so that its first
+        # first check passes.
 
         child_host_info = self.create_new_host(name=self.random_string(
                                                prefix='ChildHOST'),
@@ -60,33 +137,31 @@ class TestHostParentChild(OP5Fixture):
                                                checkinterval=2,
                                                retryinterval=2,
                                                activechecks=True,
-                                               parents=['parent_host_name'])
+                                               parents=[f'{parent_host_name}'])
 
         child_host_name = child_host_info['host_name']
 
         checkinterval = child_host_info['check_interval']
         self._hosts_to_delete.append(child_host_name)
-        self._hosts_to_delete.append(parent_host_name)
         time.sleep(5)
 
         # Sleeping till the first check of child host, which should pass
         # since its a simple ping check and the server is up.
 
-        object_type = '[hosts]'
         query = f'name="{child_host_name}"'
         next_check_in = self.how_much_to_sleep(
             object_type=object_type, query=query
         )
         logging.info(f'OP5 will do next check in : {next_check_in}')
-        time.sleep(next_check_in + 5)
+        time.sleep(next_check_in + 10)
 
         # Get report in terms of list views from the filter API
         full_child_query = ''.join([object_type, query])
         logging.info(full_child_query)
         columns = f'state,state_text,has_been_checked,state_type,' \
-                  f'state_type_text'
+                  f'state_type_text,plugin_output'
         logging.info(columns)
-
+        #
         r = self.fb.get_filter_query_data(query=full_child_query,
                                           columns=columns)
 
@@ -111,7 +186,7 @@ class TestHostParentChild(OP5Fixture):
                                      object_type='host')
 
         # Sleeping till the next check of child host, which should fail
-        # as there is no HTTP service available on it and OP5 should determin
+        # as there is no HTTP service available on it and OP5 should determine
         # it as down.
 
         next_check_in = self.how_much_to_sleep(
@@ -128,78 +203,7 @@ class TestHostParentChild(OP5Fixture):
         list_view_data = r.json()[0]
         logging.info(list_view_data)
         assert list_view_data['has_been_checked'] == 1
-        assert list_view_data['state'] == 1
-        assert list_view_data['state_type'] == 0
-        assert list_view_data['state_text'] == 'down'
-        assert list_view_data['state_type_text'] == 'soft'
-
-        # Although the child host is down but since the parent
-        # host is up, by reachability logic, it will be considered down
-        # only.
-
-        # Lets bring the parent host down and then verify that whether the
-        # child host goes unreachable instead of down
-
-        # Lets now patch the parent host with a check
-        # that's not going to pass and will make it go down
-
-        r = self.cb.patch_host_details(host_name=parent_host_name,
-                                       check_command='check_dig',
-                                       check_command_args='spinup.com')
-        assert r.status_code == 200
-
-        self.commit_to_configuration(change_type='change',
-                                     object_name=parent_host_name,
-                                     object_type='host')
-
-        # Sleeping till the next check
-
-        next_check_in_for_parent = self.how_much_to_sleep(
-            object_type=object_type, query=f'name="{parent_host_name}"'
-        )
-        logging.info(f'OP5 will do next check in : {next_check_in}')
-        time.sleep(next_check_in + 10)
-
-        # Get report in terms of list views from the filter API
-        full_parent_query = ''.join(
-            [object_type, f'name="{parent_host_name}"']
-        )
-        logging.info(full_parent_query)
-        columns = f'state,state_text,has_been_checked,state_type,' \
-                  f'state_type_text'
-        logging.info(columns)
-
-        r = self.fb.get_filter_query_data(query=full_parent_query,
-                                          columns=columns)
-
-        assert r.status_code == 200
-        list_view_data = r.json()[0]
-        logging.info(list_view_data)
-        assert list_view_data['has_been_checked'] == 1
-        assert list_view_data['state'] == 1
-        assert list_view_data['state_type'] == 0
-        assert list_view_data['state_text'] == 'down'
-        assert list_view_data['state_type_text'] == 'soft'
-
-        # Now in the next check for the child host, which is already down
-        # OP5 is going to define it as unreachable since the parent
-        # host is down as well
-
-        next_check_in = self.how_much_to_sleep(
-            object_type=object_type, query=query
-        )
-        logging.info(f'OP5 will do next check in : {next_check_in}')
-        time.sleep(next_check_in + 5)
-
-        # Get report in terms of list views from the filter API
-        r = self.fb.get_filter_query_data(query=full_child_query,
-                                          columns=columns)
-
-        assert r.status_code == 200
-        list_view_data = r.json()[0]
-        logging.info(list_view_data)
-        assert list_view_data['has_been_checked'] == 1
         assert list_view_data['state'] == 2
         assert list_view_data['state_type'] == 0
         assert list_view_data['state_text'] == 'unreachable'
-        assert list_view_data['state_type_text'] == 'hard'
+        assert list_view_data['state_type_text'] == 'soft'
